d3fb5b5be95764aee87eb14b7b9c04c860ebd83e
Nicolas NSSM <nicolas.nussbaum@knpslabs.com>
2017-01-05 17:07:53 +0100

    Made a service for Client

--- src/AppBundle/Packagist/Client.php
+++ src/AppBundle/Packagist/Client.php
@@ -7,13 +7,19 @@ class Client
     const PACKAGES_LIST_API = "https://packagist.org/packages/list.json";
     const PACKAGE_INFO_API = "https://packagist.org/p/";
 
+    private $browser;
+
+    public function __construct(Browser $browser)
+    {
+        $this->browser = $browser;
+    }
+
     /**
      * @return array
      */
     public function getPackagesList()
     {
-        $browser = new Browser();
-        $json_response = $browser->get(self::PACKAGES_LIST_API)->getContent();
+        $json_response = $this->browser->get(self::PACKAGES_LIST_API)->getContent();
         $response = json_decode($json_response, true);
 
         return $response['packageNames'];
@@ -25,8 +31,7 @@ class Client
     public function getPackageInfo($packageName)
     {
         $packageInfo = null;
-        $browser = new Browser();
-        $json_response = $browser->get(self::PACKAGE_INFO_API.$packageName.'.json')->getContent();
+        $json_response = $this->browser->get(self::PACKAGE_INFO_API.$packageName.'.json')->getContent();
         $response = json_decode($json_response, true);
 
         if (!isset($response['error'])) {
